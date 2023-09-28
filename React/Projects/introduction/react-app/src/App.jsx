fe2060349f7ffbc40aed90fe7c0e78493df0b807
Silas Lelei <writer.silas@gmail.com>
2023-09-28 18:21:09 +0300

    minor bug fixes

--- React/Projects/introduction/react-app/src/App.jsx
+++ React/Projects/introduction/react-app/src/App.jsx
@@ -6,8 +6,8 @@ import { useState, useEffect } from "react";
 import AddItem from "./components/AddItem.jsx";
 import apiRequest from "./components/apiRequest.jsx";
 
-import "./bootstrap/css/bootstrap.min.css";
-import "./bootstrap/js/bootstrap.min.js";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
 
 function App() {
   const API_URL = "http://localhost:3000/items";
